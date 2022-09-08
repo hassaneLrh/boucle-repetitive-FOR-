bdae20735653c71506365797a7d4038fc60427d6
unknown <larhouatihassane8@gmail.com>
2022-09-08 13:55:23 +0100

    premier commit

---
+++ boucle repetitive FOR.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int S , i ;
+    S=0;
+    for(i=1;i<=20;i++){
+        S = S + i;
+    }
+    printf("la somm edes 20 premiers entier est : %d " ,S);
+
+return 0;
+}
+
